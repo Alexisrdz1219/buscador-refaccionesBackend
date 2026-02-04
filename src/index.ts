ab7d6f9d7cdf9a75a4380593b4f48e12fb8226ec
Alexisrdz1219 <ealexisrdz19@gmail.com>
2026-02-03 20:28:54 -0600

    Primer subida render backend

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+app.get("/test", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result.rows[0]);
+});
+
+app.listen(5000, () => {
+  console.log("Backend vivo en puerto 5000");
+});
